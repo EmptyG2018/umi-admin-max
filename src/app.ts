8a8cdd134192dfec6859824f8be74696dd8ebc34
emptyg2018 <empty.g.2018@gmail.com>
2022-09-21 13:37:12 +0000

    --no-verify

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,36 @@
+const PageLoading = () => {
+    return <div>loading...</div>
+}
+
 // 运行时配置
+export const initialStateConfig = {
+  loading: <PageLoading />
+};
 
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://next.umijs.org/docs/api/runtime-config#getinitialstate
 export async function getInitialState(): Promise<{ name: string }> {
+    console.log("getInitialState");
   return { name: '@umijs/max' };
+}
+
+
+export function onRouteChange({ location, clientRoutes, routes, action }) {
+  console.log("onRouteChange");
+}
+
+export function patchRoutes({ routes, routeComponents }) {
+  console.log('patchRoutes');
+}
+
+export function patchClientRoutes({ routes }) {
+  console.log("patchClientRoutes");
+}
+
+export function render(oldRender) {
+    
+    console.log('oldRender');
+    oldRender();
 }
 
 export const layout = () => {
