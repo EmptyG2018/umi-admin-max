c6a6e8cd7ae81b4c6d3df493031c37a6b15aad82
EmptyG2018 <Empty.G.2018@Gmail.com>
2022-09-23 01:00:00 +0800

    --no-verify

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,11 @@
 // 运行时配置
-import { RunTimeLayoutConfig } from '@umijs/max';
 import services from '@/services/demo';
+import { history } from '@umijs/max';
+import { RunTimeLayoutConfig } from '@umijs/max';
 
 const { getProfile } = services.UserController;
+
+const loginPath = "/login";
 
 /** 获取用户信息比较慢的时候会展示一个 loading */
 // export const initialStateConfig = {
@@ -14,8 +17,8 @@ const delay = () => {
     setTimeout(() => {
       resolve();
     }, 5000);
-  })
-}
+  });
+};
 
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://next.umijs.org/docs/api/runtime-config#getinitialstate
@@ -23,7 +26,6 @@ export async function getInitialState(): Promise<{
   profile?: API.Profile_;
   getProfile?: () => Promise<API.Result_Profile_>;
 }> {
-
   // await delay();
 
   const profileOk = await getProfile();
@@ -33,40 +35,38 @@ export async function getInitialState(): Promise<{
   };
 }
 
-export const layout: RunTimeLayoutConfig = ({ initialState }) => {
+export function patchClientRoutes({ routes }: any) {
+  // 根据 extraRoutes 对 routes 做一些修改
+  console.log('app:patchClientRoutes routes', routes);
+}
 
-  console.log("initialState", initialState);
+export function render(oldRender: () => void) {
+  console.log('app:render');
+  oldRender();
+}
+
+
+export const layout: RunTimeLayoutConfig = ({ initialState }) => {
+  console.log('initialState', initialState);
 
   return {
-    logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
-    title: '主应用',
+    fixSiderbar: true,
+    layout: 'mix',
+
     menu: {
       locale: false,
     },
-    token:{
-      colorBgAppListIconHover: 'rgba(0,0,0,0.06)',
-      colorTextAppListIconHover: 'rgba(255,255,255,0.95)',
-      colorTextAppListIcon: 'rgba(255,255,255,0.85)',
-      sider: {
-        colorBgCollapsedButton: '#fff',
-        colorTextCollapsedButtonHover: 'rgba(0,0,0,0.65)',
-        colorTextCollapsedButton: 'rgba(0,0,0,0.45)',
-        colorMenuBackground: '#5b55ff',
-        colorBgMenuItemCollapsedHover: 'rgba(0,0,0,0.06)',
-        colorBgMenuItemCollapsedSelected: 'rgba(0,0,0,0.15)',
-        colorMenuItemDivider: 'rgba(255,255,255,0.15)',
-        colorBgMenuItemHover: 'rgba(0,0,0,0.06)',
-        colorBgMenuItemSelected: 'rgba(0,0,0,0.15)',
-        colorTextMenuSelected: '#fff',
-        colorTextMenu: 'rgba(255,255,255,0.75)',
-        colorTextMenuSecondary: 'rgba(255,255,255,0.65)',
-        colorTextMenuTitle: 'rgba(255,255,255,0.95)',
-        colorTextMenuActive: 'rgba(255,255,255,0.95)',
-        colorTextSubMenuSelected: '#fff',
-      },
-    },
+
     onPageChange: () => {
-      console.log('gewageg');
+      const { location } = history;
+      if (!initialState.profile && location.pathname !== loginPath) {
+        history.push(loginPath);
+      }
+    },
+
+    // 退出登录业务逻辑
+    logout: () => {
+      alert('退出登录');
     },
   };
 };
